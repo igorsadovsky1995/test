5726e7fcc7397d250cf3c7369dad5650f271844a
igorsadovsky1995 <79226207+igorsadovsky1995@users.noreply.github.com>
2022-07-27 21:11:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+   function changeBlock(){
+        if(window.innerWidth < 992){
+            $('.col-site__item').appendTo('.site-footer__content');
+        }
+        if(window.innerWidth > 992){
+            $('.col-site__item').appendTo('.col-site');
+        }
+   }
+   changeBlock();
+   $(window).resize(changeBlock);
+
+    $(window).on('load',function(){
+   
+        function loaded(){
+            
+            $('.loading').addClass('hide');
+            setTimeout(bodyScroll,1500);
+            function bodyScroll(){
+                $('body').css({"overflow":"auto"})
+            }
+        }
+        setTimeout(loaded,3800)
+
+        
+    })
+   
+   
+})
