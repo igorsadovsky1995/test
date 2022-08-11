4a9bb2fa263d9f8f860d0a60e7e920819508d62a
igorsadovsky1995 <79226207+igorsadovsky1995@users.noreply.github.com>
2022-08-11 23:41:25 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    function fullScreen(){
-        let heaghtBlock=$(window).outerHeight(true) - $('.site-header').outerHeight(true);
-        $('.mechanization__body').css({'min-height':''+heaghtBlock+'px'});
-    }
-    fullScreen()
-    $(window).resize(fullScreen);
-})
